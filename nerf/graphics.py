fe72319283f5d2f11898d374ff339ec65df24984
cc <cctom@umich.edu>
2022-04-24 00:50:13 -0400

    Add nerf_iter_once

--- nerf/graphics.py
+++ nerf/graphics.py
@@ -1,33 +1,34 @@
 import torch
 from torch import Tensor
+import torch.nn.functional as F
 
-def compute_rays(img_h:int, img_w:int, int_mat:Tensor, mat_c2w:Tensor) -> tuple[torch.Tensor, torch.Tensor]:
+def compute_rays(img_dim: tuple[int, int], int_mat:Tensor, mat_c2w:Tensor) -> tuple[torch.Tensor, torch.Tensor]:
     """
-    TODO: change meshgrid
     Compute rays for each pixel of an image
 
     Args:
-        img_h (int): image height
-        img_w (int): image width
+        img_dim (tuple[int, int]): image dimension (h,w)
         int_mat (Tensor): camera intrinsic matrix
         mat_c2w (Tensor): transformation matrix from camera to world frame
 
     Returns:
         tuple of (
-            rays_o: origin of the output rays Nx3
-            rays_d: direction vector of the output rays Nx3
+            rays_o: origin of the output rays HxWx3
+            rays_d: direction vector of the output rays HxWx3
         )
     """
-    i, j = torch.meshgrid(torch.linspace(0, img_w-1, img_w), torch.linspace(0, img_h-1 ,img_h))
-    # transpose and change device
-    i = i.T.to(int_mat)
-    j = j.T.to(int_mat)
+    img_h, img_w = img_dim
+    i, j = torch.meshgrid(torch.linspace(0, img_w-1, img_w), torch.linspace(0, img_h-1 ,img_h), indexing="xy")
+    # change device
+    i = i.to(int_mat)
+    j = j.to(int_mat)
     # pixel -> cam frame
     fx, fy = int_mat[0,0], int_mat[1,1]
     cx, cy = int_mat[0,2], int_mat[1,2]
     rays_d:Tensor = torch.stack([(i-cx)/fx, -(j - cy)/fy, -torch.ones_like(i)]).permute(1,2,0)
     # cam frame -> world
     rays_d = rays_d[..., None, :].matmul(mat_c2w[:3,:3])[:,:,0,:]
+    rays_d = F.normalize(rays_d, dim=2)
     
     # origin of the rays
     rays_o = mat_c2w[:3,-1].expand(rays_d.shape)
@@ -36,73 +37,72 @@ def compute_rays(img_h:int, img_w:int, int_mat:Tensor, mat_c2w:Tensor) -> tuple[
 
 
 def queries_from_rays(
-	rays_ori_xyz: torch.Tensor,
-	rays_dir_xyz: torch.Tensor,
-	t_n: float,
-	t_f: float,
-	num_samples: int
-	) -> tuple[torch.Tensor, torch.Tensor]:
-	"""Compute the input queries to the NeRF model for the given rays in an image of shape (H,W)
-
-	Args:
-		rays_ori_xyz (torch.Tensor): starting location for each ray, shape (W, H, 3)
-		rays_dir_xyz (torch.Tensor): normalized direction for each ray, shape (W, H, 3)
-		t_n (float): nearest threshold
-		t_f (float): farthest threhold
-		num_samples (int): number of samples to generate
-
-	Returns:
-		tuple[torch.Tensor, torch.Tensor]:
-			query points along each ray, shape (W, H, num_samples, 3)
-			sampled depth value for each ray (num_samples)
-	"""
-	this_device = rays_dir_xyz.device
-
-	# generate depths
-	uniform_sample_depths = torch.linspace(t_n, t_f, num_samples, device=this_device)
-	depth_noise = uniform_sample_depths + torch.rand(uniform_sample_depths.shape, device=this_device)*(t_f-t_n)/(2*num_samples)
-	noisy_sample_depth = uniform_sample_depths + depth_noise
-
-	# update query input point positions
-	query_points_xyz =  rays_ori_xyz.unsqueeze(dim=2) + rays_dir_xyz.unsqueeze(dim=2)* noisy_sample_depth.unsqueeze(dim=-1)
-	return query_points_xyz, noisy_sample_depth
+    rays_ori_xyz: torch.Tensor,
+    rays_dir_xyz: torch.Tensor,
+    sample_thresh: tuple[float, float],
+    num_samples: int
+    ) -> tuple[torch.Tensor, torch.Tensor]:
+    """Compute the input queries to the NeRF model for the given rays in an image of shape (H,W)
+    
+    Args:
+    	rays_ori_xyz (torch.Tensor): starting location for each ray, shape (W, H, 3)
+    	rays_dir_xyz (torch.Tensor): normalized direction for each ray, shape (W, H, 3)
+        sample_thresh (tuple[float, float]): (nearest, farthest) threshold
+    	num_samples (int): number of samples to generate
+    
+    Returns:
+    	tuple[torch.Tensor, torch.Tensor]:
+    		query points along each ray, shape (W, H, num_samples, 3)
+    		sampled depth value for each ray (num_samples)
+    """
+    this_device = rays_dir_xyz.device
+    t_n, t_f = sample_thresh
+    
+    # generate depths
+    uniform_sample_depths = torch.linspace(t_n, t_f, num_samples, device=this_device)
+    depth_noise = uniform_sample_depths + torch.rand(uniform_sample_depths.shape, device=this_device)*(t_f-t_n)/(2*num_samples)
+    noisy_sample_depth = uniform_sample_depths + depth_noise
+    
+    # update query input point positions
+    query_points_xyz =  rays_ori_xyz.unsqueeze(dim=2) + rays_dir_xyz.unsqueeze(dim=2)* noisy_sample_depth.unsqueeze(dim=-1)
+    return query_points_xyz, noisy_sample_depth
 
 
 def render_from_nerf(
-	nerf_output: torch.Tensor,
-	rays_ori_xyz: torch.Tensor,
-	sampled_depths: torch.Tensor
-	)-> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
-	""" Compute the rendering result from the output of NeRF
-
-	Args:
-		nerf_output (torch.Tensor): nerf output in rgba, shape (W, H, 4)
-		rays_ori_xyz (torch.Tensor): starting location for each ray, shape (W, H, 3)
-		sampled_depths (torch.Tensor): sampled depth value for each ray (num_samples)
-
-	Returns:
-		tuple[torch.Tensor, torch.Tensor, torch.Tensor]: 
-			rgb_img, shape (W, H, 3)
-			depth_img, shape (W, H)
-	"""	
-	# normalize the nerf output
-	normed_raw_alpha_maps = F.relu(nerf_output[..., 3])
-	rgb_maps = F.sigmoid(nerf_output[..., :3])
-
-	# span for each sampled points by diff
-	depth_spans = sampled_depths[1:] - sampled_depths[:-1]
-	depth_spans = torch.cat(depth_spans, torch.Tensor(1e10))
-
-	# compute the weights for each point in a pixel by alpha and depth_span
-	alpha_maps = 1. - torch.exp(- normed_raw_alpha_maps * depth_spans)
-
-	# get exp(-\sum \delta * \sigma)
-	cum_prods = torch.cumprod(1-alpha_maps + 1e-10, dim=-1)
-	cum_prods = cum_prods.roll(shifts=1, dim=-1)
-	cum_prods[..., -1] = 0
-	weights = alpha_maps * cum_prods
-
-	# results
-	rgb_img = (rgb_maps * weights.unsqueeze(-1)).sum(dim=-2)
-	depth_img = (sampled_depths * weights).sum(dim=-1)
-	return rgb_img, depth_img
+    nerf_output: torch.Tensor,
+    rays_ori_xyz: torch.Tensor,
+    sampled_depths: torch.Tensor
+    )-> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+    """ Compute the rendering result from the output of NeRF
+    
+    Args:
+    	nerf_output (torch.Tensor): nerf output in rgba, shape (W, H, num_samples, 4)
+    	rays_ori_xyz (torch.Tensor): starting location for each ray, shape (W, H, 3)
+    	sampled_depths (torch.Tensor): sampled depth value for each ray (num_samples)
+    
+    Returns:
+    	tuple[torch.Tensor, torch.Tensor, torch.Tensor]: 
+    		rgb_img, shape (W, H, 3)
+    		depth_img, shape (W, H)
+    """	
+    # normalize the nerf output
+    normed_raw_alpha_maps = F.relu(nerf_output[..., 3])
+    rgb_maps = F.sigmoid(nerf_output[..., :3])
+    
+    # span for each sampled points by diff
+    depth_spans = sampled_depths[1:] - sampled_depths[:-1]
+    depth_spans = torch.cat(depth_spans, torch.Tensor(1e10))
+    
+    # compute the weights for each point in a pixel by alpha and depth_span
+    alpha_maps = 1. - torch.exp(- normed_raw_alpha_maps * depth_spans)
+    
+    # get exp(-\sum \delta * \sigma)
+    cum_prods = torch.cumprod(1-alpha_maps + 1e-10, dim=-1)
+    cum_prods = cum_prods.roll(shifts=1, dim=-1)
+    cum_prods[..., -1] = 0
+    weights = alpha_maps * cum_prods
+    
+    # results
+    rgb_img = (rgb_maps * weights.unsqueeze(-1)).sum(dim=-2)
+    depth_img = (sampled_depths * weights).sum(dim=-1)
+    return rgb_img, depth_img
--- nerf/nerf_helper.py
+++ nerf/nerf_helper.py
@@ -1,9 +1,72 @@
-
-
 import torch
 import torch.utils.data
 from torch import nn
 from torch.nn import functional as F
+from torch import Tensor
+from nerf.graphics import *
+
+
+def nerf_iter_once(model: nn.Module,
+                  img_dim: tuple[int, int],
+                  int_mat: Tensor,
+                  mat_c2w: Tensor,
+                  sample_thresh: tuple[float, float],
+                  num_samples: int = 16,
+                  batch_size=10)->tuple(Tensor, Tensor):
+    """
+    iterate one time
+
+    Args:
+        model (nn.Module): nerf model to use
+        img_dim (tuple[int, int]): dimension of image (H, W)
+        int_mat (Tensor): camera intrinsic matrix
+        mat_c2w (Tensor): transformation matrix from camera to world
+        sample_thresh (tuple[float, float]): range of sampling (near, far)
+        num_samples (int, optional): number of samples points per ray. Defaults to 16.
+        batch_size (int, optional): number of pixels to train per batch. Defaults to 10.
+
+    Returns:
+        tuple of (
+            rgb image: Tensor of shape HxWx3
+            depth image: Tensor of shape HxW
+        )
+    """
+    # compute rays on all pixels of the image
+    rays_o, rays_d = compute_rays(img_dim, int_mat, mat_c2w)
+
+    # Sample points on each ray
+    samples, depth_values = queries_from_rays(rays_o, rays_d, sample_thresh,
+                                              num_samples)
+
+    # flatten samples
+    samples = samples.reshape(-1, 3)
+
+    # encode points
+    # TODO: set encoding dimension as parameter
+    encoded_samples = PosEncode(samples, 10)
+    encoded_dirs = PosEncode(rays_d, 4)
+
+    # train each pixel
+    nerf_out_list: list = []
+    n = len(encoded_dirs)
+    for i in range(0, n, batch_size):
+        if (i + batch_size > n):
+            samples_crop = encoded_samples[num_samples * i:]
+            dirs_crop = encoded_dirs[i:].expand(samples_crop.shape)
+        else:
+            samples_crop = encoded_samples[num_samples * i: num_samples * (i+batch_size)]
+            dirs_crop = encoded_dirs[i:i+batch_size].expand(samples_crop.shape)
+
+        nerf_out_list.append(
+                model(samples_crop, dirs_crop))
+    
+    nerf_out = torch.cat(nerf_out_list, dim=0)
+
+    nerf_out = nerf_out.reshape(img_dim[0], img_dim[1], num_samples, 4)
+    # Perform differentiable volume rendering to re-synthesize the RGB image.
+    rgb_predicted, depth_img = render_from_nerf(nerf_out, rays_o, depth_values)
+
+    return rgb_predicted, depth_img
 
 
 def PosEncode(x, L):
@@ -21,17 +84,18 @@ def PosEncode(x, L):
         enc_x: (N, 3*2*L)
     '''
     N = x.shape[0]
-    enc_x = torch.zeros((N, 3, 2*L), dtype=x.dtype, device=x.device)
+    enc_x = torch.zeros((N, 3, 2 * L), dtype=x.dtype, device=x.device)
 
     # Normalize x
     sum_x = torch.sqrt(torch.sum(x**2, dim=1)).reshape(N, 1)
     x /= sum_x
-    
+
     # encode
     freq_band = 2**torch.floor(torch.arange(0, L, 0.5, device=x.device))
-    freq_t_x = freq_band.reshape(1, -1) * x.reshape(N, -1, 1)  # freq_band*x (N, 3, 2L)
+    freq_t_x = freq_band.reshape(1, -1) * x.reshape(
+        N, -1, 1)  # freq_band*x (N, 3, 2L)
 
-    enc_x[:,:,0::2] = torch.sin(freq_t_x[:, :,0::2])
-    enc_x[:,:,1::2] = torch.cos(freq_t_x[:, :,1::2])
+    enc_x[:, :, 0::2] = torch.sin(freq_t_x[:, :, 0::2])
+    enc_x[:, :, 1::2] = torch.cos(freq_t_x[:, :, 1::2])
     enc_x = torch.flatten(enc_x, start_dim=1)
     return enc_x
